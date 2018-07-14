28bf10b23842f9eb582aaf48c57928e055a85d17
AidenPL <aiden.lynch22@gmail.com>
2018-07-14 15:47:09 +0100

    Added link to the Nav Title to take you home

--- src/Comnponents/NavBar/index.js
+++ src/Comnponents/NavBar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //import NavItem from './NavItem'
-//import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import JobAdd from '../JobAdd'
 
 class NavBar extends React.Component{
@@ -19,13 +19,14 @@ class NavBar extends React.Component{
 
         return(
     
-            <nav className = "navbar navbar-expand-md navbar-dark bg-dark" style={{height: '30px'}}>
-                <div className="navbar-brand">Workshop Manager</div>
+            <nav className = "navbar navbar-expand-md navbar-dark bg-dark" style={{height: '35px'}}>
+                <Link to= {'/'}><div className="navbar-brand">Workshop Manager</div></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
+                    {/* <Link to={'/'}><button type="button" className="btn-sm btn-outline-light" style={{marginRight: '10px'}}>Home</button></Link> */}
                         <JobAdd />
                     </div>
                 </div>
